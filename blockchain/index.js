e2385c07248caa86b1a55e840e3d6ea79fa236e1
Callum <>
2021-03-28 23:21:56 +0200

    Improvement | clean blockchain/index.js

--- blockchain/index.js
+++ blockchain/index.js
@@ -14,22 +14,9 @@ class Blockchain {
                 this.chain.push(block);
 
             return resolve();
-            }).catch(error => reject(error))
+            }).catch(reject);
         });
     }
 }
 
 module.exports = Blockchain;
-
-const blockchain = new Blockchain();
-
-for (let i = 0; i<1000; i++) {
-    const lastBlock = blockchain.chain[blockchain.chain.length - 1];
-    const block = Block.mineBlock({
-        lastBlock,
-        beneficiary: 'beneficiary'
-    });
-    blockchain.addBlock({ block })
-
-    console.log('block', block);
-}
